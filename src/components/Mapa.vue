ef99a525ff0563a2fd6f8a72d1f4a2c16f61bae9
m-dantas <mauricio.dantascp@gmail.com>
2020-12-16 15:01:11 -0300

    feat: inclui url dos centros abertos

--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -50,40 +50,84 @@
 						/>
 					</vl-style-box>
 				</vl-feature>
-			</vl-layer-vector>
 
-			<vl-interaction-select :features.sync="selectedFeatures"></vl-interaction-select>
-			<vl-overlay
-			 	v-for="feature in selectedFeatures"
-				:key="feature.properties.id"
-        :id="feature.properties.id"
-        :position="feature.geometry.coordinates"
-				:auto-pan="true"
-        :auto-pan-animation="{ duration: 300 }"
-      >
-				<template>
-					<section class="mapa__card">
-            <header
-							class="mapa__card-header"
-							:style="`background-image: url(${require(`../assets/capas/${feature.properties.capa}`)})`"
-						></header>
-            <div class="mapa__card-content">
-							<ul>
-								<li class="lista-nome">
-									{{ feature.properties.nome }}
-								</li>
-								<li>
-									{{ feature.properties.implantado ? 'Implantado' : 'Em andamento' }}
-								</li>
-								<li>
-									<span>Saiba mais</span>
-								</li>
-							</ul>
-            </div>
-					</section>
-				</template>
-			</vl-overlay>
-    </vl-map>
+				<vl-interaction-select :features.sync="selectedFeatures"></vl-interaction-select>
+				<vl-overlay
+					v-for="feature in selectedFeatures"
+					:key="feature.properties.id"
+					:id="feature.properties.id"
+					:position="feature.geometry.coordinates"
+					:auto-pan="true"
+					:auto-pan-animation="{ duration: 300 }"
+				>
+					<template>
+						<section class="mapa__card">
+							<header
+								class="mapa__card-header"
+								:style="`background-image: url(${require(`../assets/capas/${feature.properties.capa}`)})`"
+							></header>
+							<div class="mapa__card-content">
+								<ul>
+									<li class="lista-nome">
+										<svg
+											width="15"
+											height="15"
+											viewBox="0 0 14 20"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												fill-rule="evenodd"
+												clip-rule="evenodd"
+												fill="black"
+												fill-opacity="0.54"
+												d="M0 7.26501C0 3.39502 3.13 0.265015 7 0.265015C10.87 0.265015 14 3.39502 14 7.26501C14 11.435 9.58002 17.185 7.77002 19.375C7.37 19.855 6.64001 19.855 6.23999 19.375C4.41998 17.185 0 11.435 0 7.26501ZM4.5 7.26501C4.5 8.64502 5.62 9.76501 7 9.76501C8.38 9.76501 9.5 8.64502 9.5 7.26501C9.5 5.88501 8.38 4.76501 7 4.76501C5.62 4.76501 4.5 5.88501 4.5 7.26501Z"
+											/>
+										</svg>
+										<span>{{ feature.properties.nome }}</span>
+									</li>
+									<li>
+										<svg
+											width="15"
+											height="15"
+											viewBox="0 0 16 16"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												fill-rule="evenodd"
+												clip-rule="evenodd"
+												fill="black"
+												fill-opacity="0.54"
+												d="M14 0C14.83 0 15.5 0.669998 15.5 1.5V14.5C15.5 15.33 14.83 16 14 16C13.17 16 12.5 15.33 12.5 14.5V1.5C12.5 0.669998 13.17 0 14 0ZM2 10C2.82999 10 3.5 10.67 3.5 11.5V14.5C3.5 15.33 2.82999 16 2 16C1.17001 16 0.5 15.33 0.5 14.5V11.5C0.5 10.67 1.17001 10 2 10ZM9.5 6.5C9.5 5.67 8.82999 5 8 5C7.17001 5 6.5 5.67 6.5 6.5V14.5C6.5 15.33 7.17001 16 8 16C8.82999 16 9.5 15.33 9.5 14.5V6.5Z"
+											/>
+										</svg>
+
+										<span>{{ feature.properties.implantado ? 'Implantado' : 'Em andamento' }}</span>
+									</li>
+									<li>
+										<svg
+											width="15"
+											height="15"
+											viewBox="0 0 600 600"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												fill="black"
+												fill-opacity="0.54"
+												d="M533.333 533.333H66.6667V66.6667H300V0H66.6667C29.6667 0 0 30 0 66.6667V533.333C0 570 29.6667 600 66.6667 600H533.333C570 600 600 570 600 533.333V300H533.333V533.333ZM366.667 0V66.6667H486.333L158.667 394.333L205.667 441.333L533.333 113.667V233.333H600V0H366.667Z"
+											/>
+										</svg>
+										<a :href="feature.properties.url">Saiba mais</a>
+									</li>
+								</ul>
+							</div>
+						</section>
+					</template>
+				</vl-overlay>
+			</vl-layer-vector>
+		</vl-map>
 		<div class="list">
 			<div class="card">
 				<a @click="expandChatList">
@@ -156,7 +200,7 @@ export default {
 	data () {
     return {
       zoom: 11,
-			minZoom: 11,
+			minZoom: 11.4,
       center: [-46.5614286546911, -23.58172651568904],
 			rotation: 0,
 			features: [],
