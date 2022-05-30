079f08b16a23f665d417ee770ff21f23cda08c93
vgiuliani11 <vinicius.giuliani@gmail.com>
2022-05-30 11:15:26 -0300

    Especifica versão do vue e vuelayers Foi verificado que houve mudança na estruturação dos arquivos desses projetos, o que causava erro 404. Foi especificado a última versão antes dessa mudança, e aparentemente o projeto está funcionando adequadamente

--- wordpress/page-centro-aberto.php
+++ wordpress/page-centro-aberto.php
@@ -6,7 +6,7 @@ Template Name: centro-aberto
 <?php get_header(); ?>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v5.3.0/css/ol.css">
-<link rel="stylesheet" href="https://unpkg.com/vuelayers/lib/style.css">
+<link rel="stylesheet" href="https://unpkg.com/vuelayers@0.11.37/lib/style.css">
 
 <div id="default-inner">
 	<div class="wrapper">
@@ -19,7 +19,7 @@ Template Name: centro-aberto
 </div>
 
 <?php get_footer(); ?>
-<<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<<script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v5.3.0/build/ol.js"></script>
 <script src="https://unpkg.com/vuelayers/lib/index.umd.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/SPA/centro-aberto/main.min.js"></script>
